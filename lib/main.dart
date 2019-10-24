4be571ab6c148ab2a5dd940671db608f7db4e442
hvannak <vannak2010@gmail.com>
2019-10-24 11:55:31 +0700

    update login layout

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:gpstrackingplan/appsetting.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dashboard.dart';
-
 
 void main() => runApp(MyApp());
 
@@ -41,177 +41,284 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void showSnackbar(BuildContext context) {
     fetchPost();
-    final snackBar = SnackBar(content: Text('Pocessing data'));
-    _globalKey.currentState.showSnackBar(snackBar);
   }
 
   _loadSetting() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     setState(() {
-      _urlSetting = (prefs.getString('url') ?? '');     
+      _urlSetting = (prefs.getString('url') ?? '');
     });
   }
 
   Future<String> fetchPost() async {
+    _loadSetting();
     final headers = {'Content-Type': 'application/json'};
-    var body = {
-      'UserName': _username.text,
-      'Password': _password.text
-    };
+    var body = {'UserName': _username.text, 'Password': _password.text};
     final response =
         // await http.post('http://192.168.100.93:8184/api/ApplicationUser/Login',body: json.encode(body),headers: headers);
-        await http.post(_urlSetting + '/api/ApplicationUser/Login',body: json.encode(body),headers: headers);
+        await http.post(_urlSetting + '/api/ApplicationUser/Login',
+            body: json.encode(body), headers: headers);
     if (response.statusCode == 200) {
       Navigator.push(
-        context, 
-        MaterialPageRoute(builder: (context) => Dashboard()));
+          context, MaterialPageRoute(builder: (context) => Dashboard()));
       return response.body;
     } else {
-      print(response.statusCode);
+      final snackBar = SnackBar(content: Text('Failed to login'));
+      _globalKey.currentState.showSnackBar(snackBar);
       throw Exception('Failed to load post');
     }
   }
 
   @override
-  void initState() {
-    super.initState();
-    _loadSetting();
-  }
-
-  @override
   Widget build(BuildContext context) {
-   return Scaffold(
-     resizeToAvoidBottomPadding: false,
-     key: _globalKey,
-     body: Stack(
-       fit: StackFit.expand,
-       children: <Widget>[
-         Image.asset(
-           'assets/images/bg.jpeg',
-           fit: BoxFit.cover,
-           color: Colors.black54,
-           colorBlendMode: BlendMode.darken,
-           ),
-           Column(
-             mainAxisAlignment: MainAxisAlignment.center,
-             children: <Widget>[
-               Padding(
-                 padding: EdgeInsets.all(20.0),
-                 child: InkWell(
-                    child:Image.asset(
-                    'assets/images/user.png',
-                    color: Colors.blue,
-                    height: 180.0,
-                    width: 180.0,
-                    ),
-                    onTap: (){
-                      Navigator.push(
-                                context, 
-                                MaterialPageRoute(builder: (context) => Appsetting()));
-                    },
-                 ),
-               ),
-               Stack(
-                   children: <Widget>[
-                     SingleChildScrollView(
-                       child: Container(
-                       height: 300.0,
-                       width: 450.0,
-                       padding: EdgeInsets.symmetric(
-                         horizontal: 30.0,
-                         vertical: 20.0
-                       ),
-                       decoration: BoxDecoration(
-                         color: Colors.white,
-                         shape: BoxShape.rectangle,
-                         borderRadius: BorderRadius.circular(20.0)
-                       ),
-                       child: Column(
-                         mainAxisAlignment: MainAxisAlignment.center,
-                         children: <Widget>[
-                           Form(
-                             key: _formKey,
-                             child: Column(
-                               children: <Widget>[
-                                 Padding(
-                             padding: EdgeInsets.symmetric(
-                               vertical: 25.0
-                             ),
-                             child:TextFormField(
-                             controller: _username,
-                             validator: (val)=> val.isEmpty ? "Username is required" : null,
-                             autocorrect: false,
-                             autofocus: false,
-                             style: TextStyle(
-                               fontSize: 14.0
-                             ),
-                             decoration: InputDecoration(
-                               hintText: "Username",
-                               border: InputBorder.none,
-                               filled: true,
-                               fillColor: Colors.grey[200],
-                               contentPadding: EdgeInsets.all(15.0),
-                             ),
-                            )
-                           ),
-                           TextFormField(
-                             controller: _password,
-                             validator: (val) => val.isEmpty ? "Password is required" : null,
-                             autocorrect: false,
-                             autofocus: false,
-                             obscureText: true,
-                             style: TextStyle(
-                               fontSize: 14.0
-                             ),
-                             decoration: InputDecoration(
-                               hintText: "Password",
-                               border: InputBorder.none,
-                               filled: true,
-                               fillColor: Colors.grey[200],
-                               contentPadding: EdgeInsets.all(15.0)
-                             ),
-                            ),
-                            Padding(
-                              padding: EdgeInsets.only(top: 20.0),
-                              child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Center(
-                                  child: RaisedButton(
-                                    padding: EdgeInsets.symmetric(
-                                      vertical: 15.0
-                                  ),
-                                  onPressed: (){
-                                    if(_formKey.currentState.validate()){
-                                      
-                                      showSnackbar(context);
-                                    }
-                                  },
-                                  child: Text(
-                                    'Login',
-                                    style: TextStyle(
-                                      fontSize: 14.0
-                                      ),
-                                    ),
-                                  ),
-                                )
-                              ],
-                             )
-                            )
-                               ],
-                             ),
-                           ) 
-                         ],
-                       ),
-                     ),
-                     )
-                   ],
-                 ),
-             ],
-           )
-       ],
-     ),
-   );
+    return Scaffold(
+        resizeToAvoidBottomPadding: false,
+        key: _globalKey,
+        body: OrientationBuilder(
+            builder: (BuildContext context, Orientation orientation) {
+          return Center(
+              child: orientation == Orientation.portrait
+                  ? _veticalLayout()
+                  : _horizontalLayout());
+        }));
   }
-}
 
+  _veticalLayout() {
+    return Stack(
+      fit: StackFit.expand,
+      children: <Widget>[
+        Image.asset(
+          'assets/images/bg.jpeg',
+          fit: BoxFit.cover,
+          color: Colors.black54,
+          colorBlendMode: BlendMode.darken,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(20.0),
+              child: InkWell(
+                child: Image.asset(
+                  'assets/images/user.png',
+                  color: Colors.blue,
+                  height: 180.0,
+                  width: 180.0,
+                ),
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => Appsetting()));
+                },
+              ),
+            ),
+            Stack(
+              children: <Widget>[
+                SingleChildScrollView(
+                  child: Container(
+                    height: 300.0,
+                    width: 450.0,
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.circular(20.0)),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Form(
+                          key: _formKey,
+                          child: Column(
+                            children: <Widget>[
+                              Padding(
+                                  padding: EdgeInsets.symmetric(vertical: 25.0),
+                                  child: TextFormField(
+                                    controller: _username,
+                                    validator: (val) => val.isEmpty
+                                        ? "Username is required"
+                                        : null,
+                                    autocorrect: false,
+                                    autofocus: false,
+                                    style: TextStyle(fontSize: 14.0),
+                                    decoration: InputDecoration(
+                                      hintText: "Username",
+                                      border: InputBorder.none,
+                                      filled: true,
+                                      fillColor: Colors.grey[200],
+                                      contentPadding: EdgeInsets.all(15.0),
+                                    ),
+                                  )),
+                              TextFormField(
+                                controller: _password,
+                                validator: (val) =>
+                                    val.isEmpty ? "Password is required" : null,
+                                autocorrect: false,
+                                autofocus: false,
+                                obscureText: true,
+                                style: TextStyle(fontSize: 14.0),
+                                decoration: InputDecoration(
+                                    hintText: "Password",
+                                    border: InputBorder.none,
+                                    filled: true,
+                                    fillColor: Colors.grey[200],
+                                    contentPadding: EdgeInsets.all(15.0)),
+                              ),
+                              Padding(
+                                  padding: EdgeInsets.only(top: 20.0),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      Center(
+                                        child: RaisedButton(
+                                          padding: EdgeInsets.symmetric(
+                                              vertical: 15.0),
+                                          onPressed: () {
+                                            if (_formKey.currentState
+                                                .validate()) {
+                                              fetchPost();
+                                              // showSnackbar(context);
+                                            }
+                                          },
+                                          child: Text(
+                                            'Login',
+                                            style: TextStyle(fontSize: 14.0),
+                                          ),
+                                        ),
+                                      )
+                                    ],
+                                  ))
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
+        )
+      ],
+    );
+  }
+
+  _horizontalLayout() {
+    return Stack(
+      fit: StackFit.expand,
+      children: <Widget>[
+        Image.asset(
+          'assets/images/bg.jpeg',
+          fit: BoxFit.cover,
+          color: Colors.black54,
+          colorBlendMode: BlendMode.darken,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(20.0),
+              child: InkWell(
+                child: Image.asset(
+                  'assets/images/user.png',
+                  color: Colors.blue,
+                  height: 180.0,
+                  width: 180.0,
+                ),
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => Appsetting()));
+                },
+              ),
+            ),
+            Stack(
+              children: <Widget>[
+                SingleChildScrollView(
+                  child: Container(
+                    height: 300.0,
+                    width: 450.0,
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.circular(20.0)),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Form(
+                          key: _formKey,
+                          child: Column(
+                            children: <Widget>[
+                              Padding(
+                                  padding: EdgeInsets.symmetric(vertical: 25.0),
+                                  child: TextFormField(
+                                    controller: _username,
+                                    validator: (val) => val.isEmpty
+                                        ? "Username is required"
+                                        : null,
+                                    autocorrect: false,
+                                    autofocus: false,
+                                    style: TextStyle(fontSize: 14.0),
+                                    decoration: InputDecoration(
+                                      hintText: "Username",
+                                      border: InputBorder.none,
+                                      filled: true,
+                                      fillColor: Colors.grey[200],
+                                      contentPadding: EdgeInsets.all(15.0),
+                                    ),
+                                  )),
+                              TextFormField(
+                                controller: _password,
+                                validator: (val) =>
+                                    val.isEmpty ? "Password is required" : null,
+                                autocorrect: false,
+                                autofocus: false,
+                                obscureText: true,
+                                style: TextStyle(fontSize: 14.0),
+                                decoration: InputDecoration(
+                                    hintText: "Password",
+                                    border: InputBorder.none,
+                                    filled: true,
+                                    fillColor: Colors.grey[200],
+                                    contentPadding: EdgeInsets.all(15.0)),
+                              ),
+                              Padding(
+                                  padding: EdgeInsets.only(top: 20.0),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      Center(
+                                        child: RaisedButton(
+                                          padding: EdgeInsets.symmetric(
+                                              vertical: 15.0),
+                                          onPressed: () {
+                                            if (_formKey.currentState
+                                                .validate()) {
+                                              fetchPost();
+                                              // showSnackbar(context);
+                                            }
+                                          },
+                                          child: Text(
+                                            'Login',
+                                            style: TextStyle(fontSize: 14.0),
+                                          ),
+                                        ),
+                                      )
+                                    ],
+                                  ))
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
+        )
+      ],
+    );
+  }
+
+}
